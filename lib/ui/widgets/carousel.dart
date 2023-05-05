ed0fa487620a231d1391e765fd93be83c4f893ee
Ameh Christopher <akrine2christophine@gmail.com>
2023-05-06 01:47:52 +0700

    create reusable for carousel

--- lib/ui/widgets/carousel.dart
+++ lib/ui/widgets/carousel.dart
@@ -1,6 +1,7 @@
-import 'package:dribble_design/ui/widgets/indicator_dot.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+
+import 'indicator_dot.dart';
 
 class Carousel extends HookWidget {
   final List<CarouselItem> children;
